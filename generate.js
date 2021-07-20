a4d8658ab7f3d210efe43891eb4b72e84dc5e0ac
Aremanda Abhijeeth <60499319+19PA1A0508@users.noreply.github.com>
2021-07-20 19:19:32 +0530

    Update generate.js

--- generate.js
+++ generate.js
@@ -3,3 +3,22 @@
 2.using random to generate positions which are only divisible by 10
 
 */
+
+class generatemissile{
+
+    generate(){
+
+        var generatemis = setInterval(() => {
+            var mis = document.createElement("div");
+            mis.classList.add("mis");
+            var x=Math.floor(Math.random() * 450) 
+            var s=x.toString();
+            var s1=s.slice(0, s.length-1);
+            s1=s1+'0'
+            s1=parseInt(s1)
+            mis.style.left = s1 + "px";
+            board.appendChild(mis);
+          }, 2000);
+    }
+
+}
