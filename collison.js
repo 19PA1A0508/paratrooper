9309c43bb5094b3ff298b5423aa86ce10c40dc0a
Aremanda Abhijeeth <60499319+19PA1A0508@users.noreply.github.com>
2021-07-20 19:20:45 +0530

    Update collison.js

--- collison.js
+++ collison.js
@@ -2,3 +2,45 @@
 1.for loop for every missile ,calculate property values
 2.collison co ordinates condition with reload
 */
+
+class collision{
+    coll(){
+        
+        var movemis = setInterval(() => {
+            var miss = document.getElementsByClassName("mis");
+            var img= document.getElementById("man")
+
+            
+              for (var i = 0; i < miss.length; i++) {
+                var mis = miss[i]; 
+                var mistop=parseInt(getComputedStyle(mis).getPropertyValue("top"))
+        
+        
+                var misleft = parseInt(getComputedStyle(mis).getPropertyValue("left"))
+                
+        
+
+                    var left = parseInt(getComputedStyle(img).getPropertyValue("left"))
+                    
+                    if(mistop==400&&Math.abs(misleft+33-left)<=5) { 
+                       
+                         alert("Game Over");
+                        
+                         clearInterval(movemis);
+
+                         location.reload()
+                       
+                        
+                      
+                        }
+
+                    }
+                },200)
+        
+                
+                }
+                
+                }
+    
+
+        
