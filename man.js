11d667a29b6f2737ee8087dffa05a32516d7bea4
Aremanda Abhijeeth <60499319+19PA1A0508@users.noreply.github.com>
2021-07-20 19:21:15 +0530

    Update man.js

--- man.js
+++ man.js
@@ -3,3 +3,26 @@
 2.moving man with keys using keydown
 
 */
+
+class Man {
+    
+        
+        movement() {
+            console.log("f")
+            var img = document.getElementById("man");
+            
+            addEventListener("keydown", function(e) {
+                var left = parseInt(window.getComputedStyle(img).getPropertyValue("left"));
+                if (e.key == "ArrowLeft" && left > 0) {
+                    img.style.left = left - 10 + "px";}
+                if (e.key == "ArrowRight" && left <= 460) {
+                    img.style.left = left + 10 + "px";
+                }
+            });
+        };
+    }
+
+
+
+
+
