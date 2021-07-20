09d3dea72fe90db849efbed8af9e0f439b64a389
Aremanda Abhijeeth <60499319+19PA1A0508@users.noreply.github.com>
2021-07-20 19:21:45 +0530

    Update missilemovement.js

--- missilemovement.js
+++ missilemovement.js
@@ -4,3 +4,28 @@ for loop to iterate
 calling collison file
 moving down missiles
 */
+
+class mismovement{
+   
+    mismovedown(){
+        var movemis = setInterval(() => {
+            var miss = document.getElementsByClassName("mis");
+              for (var i = 0; i < miss.length; i++) {
+                var mis = miss[i]; 
+               var mistop=parseInt(getComputedStyle(mis).getPropertyValue("top"))
+
+
+                if (mistop==475) {        
+                    document.getElementById("points").innerHTML=parseInt(document.getElementById("points").innerHTML) + 1;
+             }
+                
+                var obj3 = new collision()
+                obj3.coll()
+
+                mis.style.top = mistop + 25 + "px";
+                console.log("top--"+mistop)
+                // console.log("rockpos--"+misleft)
+                // console.log("man--"+left)
+              }
+    }
+        ,200)}}
